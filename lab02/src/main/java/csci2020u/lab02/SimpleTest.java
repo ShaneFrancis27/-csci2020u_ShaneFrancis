aa888fd50a56934a92f8fe2e16a2580f7388a104
ShaneFrancis27 <shane.francis@ontariotechu.net>
2021-02-02 15:34:52 -0500

    commit at the end of lab02 part1

---
+++ lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,10 @@
+package csci2020u.lab02;
+public class SimpleTest {
+    public static void main(String[] args) {
+
+        System.out.println((76/3)%26);
+        System.out.println((3/11)%26);
+        System.out.println((1/3)%26);
+        System.out.println((285/11)%26);
+    }
+}
